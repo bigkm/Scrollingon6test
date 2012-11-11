6f5b829d298412d51917bba1d4aea9d719675163
Kim Hunter <bigkm1@gmail.com>
2012-11-11 10:39:14 +1000

    added basic code to replicate scroll not continuing onto next scroll view

--- ScrollingOnSix/KMViewController.m
+++ ScrollingOnSix/KMViewController.m
@@ -14,6 +14,34 @@
 
 @implementation KMViewController
 
+- (UIScrollView *)scrollViewWithBackground:(UIColor *)color andContentSize:(CGSize)contentSize
+{
+    
+
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectZero];
+    scrollView.backgroundColor = color;
+    scrollView.contentSize = contentSize;
+    scrollView.showsVerticalScrollIndicator = YES;
+    
+    UIView *footerView = [[UIView alloc] initWithFrame:CGRectMake(0.0, contentSize.height - 20.0, contentSize.width, 20.0)];
+    footerView.backgroundColor = [UIColor grayColor];
+    [scrollView addSubview:footerView];
+    
+    return scrollView;
+}
+
+- (void)loadView
+{
+    UIScrollView *mainScrollView = [self scrollViewWithBackground:[UIColor underPageBackgroundColor] andContentSize:CGSizeMake(320.0f, 1200.0f)];
+
+    UIScrollView *first = [self scrollViewWithBackground:[UIColor viewFlipsideBackgroundColor] andContentSize:CGSizeMake(240.0, 600.0)];
+    first.frame = CGRectMake(0.0, 4.0, 240.0, 400.0);
+    first.indicatorStyle = UIScrollViewIndicatorStyleWhite;
+    [mainScrollView addSubview:first];
+    
+    self.view = mainScrollView;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
